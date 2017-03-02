265ab9dc151048a38273911808723c9d321c9b7c
rcalland <richard.calland@ipmu.jp>
2017-03-02 18:02:11 +0900

    added extra functionality

--- flexible_image_dataset.py
+++ flexible_image_dataset.py
@@ -1,13 +1,24 @@
 from __future__ import print_function
 
+import matplotlib
+matplotlib.use("Qt4Agg")
+from matplotlib import pyplot as plt
+
 import collections
 import os
+import math
 import numpy
 import chainer
 from random import shuffle
 from chainer.cuda import cupy
+import itertools
+from past.builtins import xrange
+
+
+from matplotlib.backends.backend_pdf import PdfPages
 
 from PIL import Image, ImageOps
+#from skimage import color
 
 class FlexibleImageDataset(chainer.datasets.LabeledImageDataset):
 
@@ -19,10 +30,15 @@ class FlexibleImageDataset(chainer.datasets.LabeledImageDataset):
         self.size = size
         self.withlabels = withlabels
         self.scale = scale
+        self.num_classes = None
+        self.calc_num_classes()
+
+    def calc_num_classes(self):
+        classes = [x[1] for x in self._pairs]
+        self.num_classes = len(set(classes))
 
     def get_num_classes(self):
-        classes = [x[1] for x in self._pairs]
-        return len(set(classes))
+        return self.num_classes
 
     def get_class_weights(self, gpu_id=-1):
         classes = [x[1] for x in self._pairs]
@@ -41,6 +57,44 @@ class FlexibleImageDataset(chainer.datasets.LabeledImageDataset):
         print("Number of classes: {}".format(len(set(classes))))
         if freq:
             print(collections.OrderedDict(collections.Counter(classes)))
+
+    def make_pdf(self, filename="dataset.pdf"):
+        with PdfPages(filename) as pdf:
+            if self.num_classes is None:
+                self.calc_num_classes()
+
+            for cls in range(self.num_classes):
+                print("class {} of {}".format(cls, self.num_classes))
+                samples = [x[0] for x in self._pairs if int(x[1]) is int(cls)]
+                print("class {} contains {} samples".format(cls, len(samples)))
+                if not samples:
+                    continue
+                plt.figure()
+                plt.axis('off')
+
+                isize = (64,64)#(32,32)
+                imagesize = (1024,1024)
+                grid = Image.new("RGB", imagesize, color=(255,255,255))
+                tmplt_path = "/mnt/sakuradata2/calland/software/traffic-sign-data-synthesizer/templates/GTSRB/original/{}.jpg".format(cls)
+                template = Image.open(tmplt_path)
+                template = template.resize(isize, Image.ANTIALIAS)
+                grid.paste(template, (0,0))
+
+                maximg = math.floor((imagesize[0] / isize[0]) * (imagesize[1] / isize[1]))
+
+                index = 0   
+                for i, j in itertools.product(xrange(isize[0],imagesize[0],isize[0]), xrange(1,imagesize[1],isize[1])):
+                    if index == len(samples) or index >= maximg:
+                        break
+                    im = Image.open(samples[index])
+                    im = im.resize(isize, Image.ANTIALIAS)
+                    grid.paste(im, (j,i))
+                    index += 1
+
+                plt.imshow(grid, interpolation="nearest")
+
+                pdf.savefig(dpi=96)  # saves the current figure into a pdf page
+                plt.close()
 
     def get_example(self, i, transpose=True):
         path, int_label = self._pairs[i]
@@ -62,12 +116,17 @@ class FlexibleImageDataset(chainer.datasets.LabeledImageDataset):
         
         label = numpy.array(int_label, dtype=self._label_dtype)
 
-        image *= self.scale
+        #print(image)
         image /= 255.0
+        #image = color.rgb2lab(image)
+        #print(image)
+        image *= self.scale
+        #print(image)
         image -= self.mean
 
         image = image.astype(self._dtype)
-
+        #print("mean {} std {}".format(image.mean(), image.std()))
+        #exit()
         if transpose:
             image = image.transpose(2,0,1)
 
